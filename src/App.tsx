91409231f2f3ab964b03fedc9976cf80e0e882c0
FehMacedo10 <fmacedoribeiro@outlook.com>
2025-02-13 08:17:23 -0300

    ✨feat: Integrar Tailwind CSS e implementar layout básico para páginas de Login e Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
+import Footer from './components/footer/Footer';
+import Login from './pages/Login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+
 
 function App() {
   return (
-    <Home />
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/cadastro' element={<Cadastro />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
